182510ce7a9b1b14e953d447887567e0e44eae25
Dhaval Kolhe <72658475+dhavalkolhe@users.noreply.github.com>
2024-03-02 16:46:21 +0530

    Timer, Volume Controls added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,95 @@
+import React, { useRef } from "react";
 import videos from "./assets/data.js";
+import usePlayer from "./components/usePlayer.js";
 
 function App() {
+  const videoRef = useRef(null);
+
+  const {
+    player,
+    togglePlaying,
+    toggleMute,
+    handleVolume,
+    handleSpeed,
+    handleTimeUpdate,
+    handleProgress,
+    // toggleAutoPlayNext,
+    toggleTimer,
+    // toggleFullScreen,
+  } = usePlayer(videoRef);
+
   return (
     <div className="App flex justify-center items-center">
       <div>
-        {/* <video controls width="600" height="400">
-          <source src={videos[3].sources[0]} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video> */}
+        <div>
+          <video
+            src={videos[2].source}
+            ref={videoRef}
+            onTimeUpdate={handleTimeUpdate}
+            width={400}
+            height={400}
+          />
+          {/* Controls */}
+          <div>
+            {/* Play/Pause Button */}
+            <div>
+              <button onClick={togglePlaying}>
+                {player.isPlaying ? <div>Pause</div> : <div>Play</div>}
+              </button>
+            </div>
+            {/* Progress Bar */}
+            {/* TODO: NEEDS TO BE OPTIMISED */}
+            <input
+              type="range"
+              min="0"
+              max="100"
+              value={player.progress}
+              onInput={(e) => handleProgress(e)}
+            />
+            {/* Speed Control */}
+            <select value={player.speed} onChange={(e) => handleSpeed(e)}>
+              <option value="0.25">0.25x</option>
+              <option value="0.5">0.5x</option>
+              <option value="0.75">0.75x</option>
+              <option value="1">1.x</option>
+              <option value="1.25">1.25x</option>
+              <option value="1.5">1.5x</option>
+              <option value="1.75">1.75x</option>
+              <option value="2">2x</option>
+            </select>
+            {/* Volume Slider and Mute */}
+            <input
+              type="range"
+              min="0"
+              max="100"
+              value={player.volume}
+              onInput={(e) => handleVolume(e)}
+            />
+            <button onClick={toggleMute}>
+              {player.isMuted ? <div>Unmute</div> : <div>Mute</div>}
+            </button>
+            {/* Auto Play */}
+            {/* <button onClick={toggleAutoPlayNext}>
+              {player.autoPlay ? (
+                <div>Disable AutoPlay</div>
+              ) : (
+                <div>Enable AutoPlay</div>
+              )}
+            </button> */}
+
+            {/* Timer/Duration */}
+            <button onClick={toggleTimer}>{player.timerValue}</button>
+
+            {/* Full Screen */}
+            {/* <button
+              onClick={() => {
+                toggleFullScreen();
+              }}
+            >
+              Enable Full Screen
+            </button> */}
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/usePlayer.js
+++ src/components/usePlayer.js
@@ -4,17 +4,14 @@ import { useState, useEffect } from "react";
 export default function usePlayer(element) {
   const [player, setPlayer] = useState({
     isPlaying: false,
-    autoplay: false,
     speed: 1,
     progress: 0,
-    volume: 100, //!look into
+    volume: 100,
     isMuted: false,
+    autoPlayNext: false,
+    showTimer: false,
+    timerValue: "",
   });
-
-  // Handling media not present error
-  if (!element || !element.current) {
-    throw new Error("Media element is not available.");
-  }
 
   // Toggle playing status
   const togglePlaying = () => {
@@ -32,6 +29,27 @@ export default function usePlayer(element) {
     }));
   };
 
+  // Toggle mute status
+  const toggleAutoPlayNext = () => {
+    setPlayer((prevPlayer) => ({
+      ...prevPlayer,
+      autoPlay: !prevPlayer.autoPlay,
+    }));
+  };
+
+  // Adjust volume
+  const handleVolume = (e) => {
+    const value = Number(e.target.value);
+    const newVolume = Math.max(0, Math.min(value, 100));
+    const isMuted = newVolume === 0;
+
+    setPlayer((prevPlayer) => ({
+      ...prevPlayer,
+      volume: newVolume,
+      isMuted,
+    }));
+  };
+
   // Set playback speed
   const handleSpeed = (e) => {
     const speed = Number(e.target.value);
@@ -42,7 +60,7 @@ export default function usePlayer(element) {
   // Handle time update for progress
   const handleTimeUpdate = () => {
     const progress =
-      100 * (element.current.currentTime() / element.current.duration());
+      100 * (element.current.currentTime / element.current.duration);
 
     setPlayer((prevPlayer) => ({
       ...prevPlayer,
@@ -51,7 +69,7 @@ export default function usePlayer(element) {
   };
 
   // Set progress manually
-  const handleManualProgress = (e) => {
+  const handleProgress = (e) => {
     const change = Number(e.target.value);
 
     element.current.currentTime = (element.current.duration / 100) * change;
@@ -61,22 +79,71 @@ export default function usePlayer(element) {
     }));
   };
 
+  // Handle Duration or Timer
+  const toggleTimer = () => {
+    setPlayer((prevPlayer) => ({
+      ...prevPlayer,
+      showTimer: !prevPlayer.showTimer,
+    }));
+  };
+
+  const toggleFullScreen = () => {
+    if (element.current) {
+      if (!document.fullscreenElement) {
+        element.current.requestFullscreen().catch((err) => {
+          console.error(
+            "Error attempting to enable full-screen mode:",
+            err.message
+          );
+        });
+      } else {
+        document.exitFullscreen();
+      }
+    }
+  };
+
+  // Utility function
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.round(time % 60);
+    return `${minutes}:${seconds.toString().padStart(2, "0")}`;
+  };
+
+  // Use Effects
   useEffect(() => {
-    // Update player state based on mute status
+    // Update player state based on mute status and volume
     element.current.muted = player.isMuted;
-  }, [player.isMuted, element]);
+    element.current.volume = player.volume / 100;
+  }, [player.isMuted, player.volume, element]);
 
   useEffect(() => {
     // Toggle play/pause based on isPlaying state
     player.isPlaying ? element.current.play() : element.current.pause();
   }, [player.isPlaying, element]);
 
+  useEffect(() => {
+    const duration = element.current.duration;
+    const currentTime = element.current.currentTime;
+
+    if (!isNaN(duration) && !isNaN(currentTime)) {
+      const timerValue = player.showTimer ? duration - currentTime : duration;
+      setPlayer((prevPlayer) => ({
+        ...prevPlayer,
+        timerValue: formatTime(timerValue),
+      }));
+    }
+  }, [player.showTimer, player.progress, element]);
+
   return {
     player,
     togglePlaying,
     toggleMute,
+    handleVolume,
     handleSpeed,
     handleTimeUpdate,
-    handleManualProgress,
+    handleProgress,
+    toggleAutoPlayNext,
+    toggleTimer,
+    toggleFullScreen,
   };
 }
